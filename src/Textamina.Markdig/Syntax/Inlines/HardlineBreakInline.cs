9fdd1cc98a486b710a55e404a69872653e14fd59
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2016-02-24 06:59:08 +0900

    Test with using a better HardlineBreakInline parser to speed up processing

--- src/Textamina.Markdig/Syntax/Inlines/HardlineBreakInline.cs
+++ src/Textamina.Markdig/Syntax/Inlines/HardlineBreakInline.cs
@@ -9,36 +9,41 @@ namespace Textamina.Markdig.Syntax
 
         private class ParserInternal : InlineParser
         {
+            public ParserInternal()
+            {
+                FirstChars = new[] {'\n'};
+            }
+
             public override bool Match(InlineParserState state)
             {
                 // Hard line breaks are for separating inline content within a block. Neither syntax for hard line breaks works at the end of a paragraph or other block element:
-                if (!(state.Block is ParagraphBlock))
-                {
-                    return false;
-                }
-
-                // A line break (not in a code span or HTML tag) that is preceded by two or more spaces 
-                // and does not occur at the end of a block is parsed as a hard line break (rendered in HTML as a <br /> tag)
                 var text = state.Lines;
-                int spaceCount = 0;
-                var c = text.CurrentChar;
-                while (c.IsSpaceOrTab())
-                {
-                    c = text.NextChar();
-                    spaceCount++;
-                }
-                if (c == '\\')
-                {
-                    c = text.NextChar();
-                    spaceCount = 2;
-                }
-                if (c != '\n' || spaceCount < 2)
+                if (!(state.Block is ParagraphBlock) || text.ColumnPosition == 0 || !text.PreviousChar1.IsSpace() || !text.PreviousChar2.IsSpace())
                 {
                     return false;
                 }
 
-                state.Inline = new HardlineBreakInline();
-                return true;
+                //// A line break (not in a code span or HTML tag) that is preceded by two or more spaces 
+                //// and does not occur at the end of a block is parsed as a hard line break (rendered in HTML as a <br /> tag)
+                //var text = state.Lines;
+                //int spaceCount = 0;
+                //var c = text.CurrentChar;
+                //while (c.IsSpaceOrTab())
+                //{
+                //    c = text.NextChar();
+                //    spaceCount++;
+                //}
+                //if (c == '\\')
+                //{
+                //    c = text.NextChar();
+                //    spaceCount = 2;
+                //}
+                //if (c != '\n' || spaceCount < 2)
+                //{
+                //    return false;
+                //}
+
+                return false;
             }
         }
 
