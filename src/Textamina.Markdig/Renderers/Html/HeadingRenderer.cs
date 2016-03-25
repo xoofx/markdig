0e93b5f21fff938634751c8c99e3095496800466
Alexandre Mutel <alexandre_mutel@live.com>
2016-03-25 20:58:07 +0900

    Optimize heading HtmlRenderer

--- src/Textamina.Markdig/Renderers/Html/HeadingRenderer.cs
+++ src/Textamina.Markdig/Renderers/Html/HeadingRenderer.cs
@@ -12,12 +12,24 @@ namespace Textamina.Markdig.Renderers.Html
     /// <seealso cref="Textamina.Markdig.Renderers.Html.HtmlObjectRenderer{Textamina.Markdig.Syntax.HeadingBlock}" />
     public class HeadingRenderer : HtmlObjectRenderer<HeadingBlock>
     {
+        private static readonly string[] HeadingTexts = {
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+        };
+
         protected override void Write(HtmlRenderer renderer, HeadingBlock obj)
         {
-            var heading = obj.Level.ToString(CultureInfo.InvariantCulture);
-            renderer.Write("<h").Write(heading).WriteAttributes(obj).Write(">");
+            var headingText = obj.Level > 0 && obj.Level <= 6
+                ? HeadingTexts[obj.Level - 1]
+                : "<h" + obj.Level.ToString(CultureInfo.InvariantCulture);
+
+            renderer.Write("<").Write(headingText).WriteAttributes(obj).Write(">");
             renderer.WriteLeafInline(obj);
-            renderer.Write("</h").Write(heading).WriteLine(">");
+            renderer.Write("</").Write(headingText).WriteLine(">");
         }
     }
 }
