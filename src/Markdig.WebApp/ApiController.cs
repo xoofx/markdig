874170bc1aeb9e95f822339e9599ce730615e9eb
Alexandre Mutel <alexandre_mutel@live.com>
2016-06-06 10:28:09 +0900

    Add Markdig.WebApp ASP.NET Core to provide API for BabelMark2 test at http://johnmacfarlane.net/babelmark2

---
+++ src/Markdig.WebApp/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Markdig.WebApp
+{
+    public class ApiController : Controller
+    {
+        // GET api/to_html?text=xxx
+        [Route("api/to_html")]
+        [HttpGet()]
+        public object Get([FromQuery] string text)
+        {
+            if (text == null)
+            {
+                text = string.Empty;
+            }
+
+            if (text.Length > 1000)
+            {
+                text = text.Substring(0, 1000);
+            }
+
+            var pipeline = new MarkdownPipelineBuilder().UseAllExtensions().Build();
+            var result = Markdown.ToHtml(text, pipeline);
+
+            return new {name = "markdig", html = result, version = Markdown.Version};}
+    }
+}
---
+++ src/Markdig.WebApp/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Markdig.WebApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseMvc();
+        }
+    }
+}
