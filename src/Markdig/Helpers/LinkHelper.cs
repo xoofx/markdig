2612e9d56575897495e367f5855a4fe7a5fb3542
Alexandre Mutel <alexandre_mutel@live.com>
2017-05-28 08:02:55 +0200

    Fix issue in TryParseAutolink in LinkHelper (#115)

--- src/Markdig/Helpers/LinkHelper.cs
+++ src/Markdig/Helpers/LinkHelper.cs
@@ -259,6 +259,7 @@ namespace Markdig.Helpers
                         break;
                     }
                     builder.Append(c);
+                    pc = c;
                 }
             }
             else
