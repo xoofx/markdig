26beaa81da789416ed5f6a62b5a56c89d3edb897
Alexandre Mutel <alexandre_mutel@live.com>
2017-10-27 22:08:34 +0200

    Add comments to NormalizeOptions (#155)

--- src/Markdig/Renderers/Normalize/NormalizeOptions.cs
+++ src/Markdig/Renderers/Normalize/NormalizeOptions.cs
@@ -3,8 +3,14 @@
 // See the license.txt file in the project root for more information.
 namespace Markdig.Renderers.Normalize
 {
+    /// <summary>
+    /// Defines the options used by <see cref="NormalizeRenderer"/>
+    /// </summary>
     public class NormalizeOptions
     {
+        /// <summary>
+        /// Initialize a new instance of <see cref="NormalizeOptions"/>
+        /// </summary>
         public NormalizeOptions()
         {
             SpaceAfterQuoteBlock = true;
@@ -14,14 +20,29 @@ namespace Markdig.Renderers.Normalize
             DefaultListItemCharacter = '-';
         }
 
+        /// <summary>
+        /// Adds a space after a QuoteBlock &gt;. Default is <c>true</c>
+        /// </summary>
         public bool SpaceAfterQuoteBlock { get; set; }
 
+        /// <summary>
+        /// Adds an empty line after a code block (fenced and tabbed). Default is <c>true</c>
+        /// </summary>
         public bool EmptyLineAfterCodeBlock { get; set; }
 
+        /// <summary>
+        /// Adds an empty line after an heading. Default is <c>true</c>
+        /// </summary>
         public bool EmptyLineAfterHeading { get; set; }
 
+        /// <summary>
+        /// Adds an empty line after an thematic break. Default is <c>true</c>
+        /// </summary>
         public bool EmptyLineAfterThematicBreak { get; set; }
 
+        /// <summary>
+        /// The bullet character used for list items. Default is <c>-</c>
+        /// </summary>
         public char DefaultListItemCharacter { get; set; }
     }
 }
