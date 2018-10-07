1b54bb157c756914809455b26273ad1a83719e57
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-10-07 19:24:24 +0200

    Added image alt text tests

---
+++ src/Markdig.Tests/TestImageAltText.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Markdig.Tests
+{
+    [TestFixture]
+    public class TestImageAltText
+    {
+        [Test]
+        [TestCase("![](image.jpg)", "")]
+        [TestCase("![foo](image.jpg)", "foo")]
+        [TestCase("![][1]\n\n[1]: image.jpg", "")]
+        [TestCase("![bar][1]\n\n[1]: image.jpg", "bar")]
+        [TestCase("![](image.jpg 'title')", "")]
+        [TestCase("![foo](image.jpg 'title')", "foo")]
+        [TestCase("![][1]\n\n[1]: image.jpg 'title'", "")]
+        [TestCase("![bar][1]\n\n[1]: image.jpg 'title'", "bar")]
+        public void TestImageHtmlAltText(string markdown, string expectedAltText)
+        {
+            string html = Markdown.ToHtml(markdown);
+            string actualAltText = Regex.Match(html, "alt=\"(.*?)\"").Groups[1].Value;
+            Assert.AreEqual(expectedAltText, actualAltText);
+        }
+    }
+}
