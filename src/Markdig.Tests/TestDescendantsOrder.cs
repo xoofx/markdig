f1b736f91825f289b60723e123b46f1a1195e1ec
Miha Zupan <mihazupan.zupan1@gmail.com>
2019-01-18 19:19:42 +0100

    Add Test for Descendants order

---
+++ src/Markdig.Tests/TestDescendantsOrder.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Markdig.Tests
+{
+    [TestFixture]
+    public class TestDescendantsOrder
+    {
+        [Test]
+        [TestCase("Foo *bar* ***foo***\n\nHello world\n\n# Header 123\n\n`Something`\n\n1. Test `abc` *def*\n2. ghi\n```c#\nEmpty\n```\nTest\n")]
+        // Note - Tested agains the entire tests dataset when implemented
+        public void AssertSameDescendantsOrder(string markdown)
+        {
+            var syntaxTree = Markdown.Parse(markdown, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+            var descendants_legacy = Descendants_Legacy(syntaxTree).ToList();
+            var descendants_new = syntaxTree.Descendants().ToList();
+
+            Assert.AreEqual(descendants_legacy.Count, descendants_new.Count);
+
+            for (int i = 0; i < descendants_legacy.Count; i++)
+            {
+                Assert.AreSame(descendants_legacy[i], descendants_new[i]);
+            }
+        }
+
+        private static IEnumerable<MarkdownObject> Descendants_Legacy(MarkdownObject markdownObject)
+        {
+            // TODO: implement a recursiveless method
+
+            var block = markdownObject as ContainerBlock;
+            if (block != null)
+            {
+                foreach (var subBlock in block)
+                {
+                    yield return subBlock;
+
+                    foreach (var sub in Descendants_Legacy(subBlock))
+                    {
+                        yield return sub;
+                    }
+
+                    // Visit leaf block that have inlines
+                    var leafBlock = subBlock as LeafBlock;
+                    if (leafBlock?.Inline != null)
+                    {
+                        foreach (var subInline in Descendants_Legacy(leafBlock.Inline))
+                        {
+                            yield return subInline;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var inline = markdownObject as ContainerInline;
+                if (inline != null)
+                {
+                    var child = inline.FirstChild;
+                    while (child != null)
+                    {
+                        var next = child.NextSibling;
+                        yield return child;
+
+                        foreach (var sub in Descendants_Legacy(child))
+                        {
+                            yield return sub;
+                        }
+
+                        child = next;
+                    }
+                }
+            }
+        }
+    }
+}
