25bc0b8bf61bcca9a5bde1154cd79ebba88f5218
Manuel Guilbault <mguilbault@la-francaise.com>
2019-04-01 15:27:17 +0200

    Set Count to zero upon Clear() calls.

--- src/Markdig/Syntax/ContainerBlock.cs
+++ src/Markdig/Syntax/ContainerBlock.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
@@ -98,6 +98,8 @@ namespace Markdig.Syntax
                 children[i].Parent = null;
                 children[i] = null;
             }
+
+            Count = 0;
         }
 
         public bool Contains(Block item)
