5b88dbb90a00668d82555f2e2c45ec4fd31c05ad
Miha Zupan <mihazupan.zupan1@gmail.com>
2019-04-10 22:59:54 +0200

    Use StringBuilderCache in JiraLink parser

--- src/Markdig/Extensions/JiraLinks/JiraLinkInlineParser.cs
+++ src/Markdig/Extensions/JiraLinks/JiraLinkInlineParser.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 using System;
@@ -92,7 +92,7 @@ namespace Markdig.Extensions.JiraLinks
             jiraLink.Span.End = jiraLink.Span.Start + (endIssue - startKey);
 
             // Builds the Url
-            var builder = new StringBuilder();
+            var builder = StringBuilderCache.Local();
             builder.Append(_baseUrl).Append('/').Append(jiraLink.ProjectKey).Append('-').Append(jiraLink.Issue);
             jiraLink.Url = builder.ToString();
 
