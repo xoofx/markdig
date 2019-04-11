210a6612c7ff9ea5b820e1363c672770e3e6880d
Miha Zupan <mihazupan.zupan1@gmail.com>
2019-04-11 11:01:37 +0200

    [CM 0.29] Apply new entity length limits

--- src/Markdig.Tests/TestHtmlHelper.cs
+++ src/Markdig.Tests/TestHtmlHelper.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 using NUnit.Framework;
 using Markdig.Helpers;
-using Markdig.Syntax;
 
 namespace Markdig.Tests
 {
@@ -15,8 +14,7 @@ namespace Markdig.Tests
         {
             var inputTag = "<a>";
             var text = new StringSlice(inputTag);
-            string outputTag;
-            Assert.True(HtmlHelper.TryParseHtmlTag(text, out outputTag));
+            Assert.True(HtmlHelper.TryParseHtmlTag(text, out string outputTag));
             Assert.AreEqual(inputTag, outputTag);
         }
 
@@ -25,9 +23,15 @@ namespace Markdig.Tests
         {
             var inputTag = "<a href='http://google.com'>";
             var text = new StringSlice(inputTag);
-            string outputTag;
-            Assert.True(HtmlHelper.TryParseHtmlTag(text, out outputTag));
+            Assert.True(HtmlHelper.TryParseHtmlTag(text, out string outputTag));
             Assert.AreEqual(inputTag, outputTag);
+        }
+
+        [Test]
+        public void DoNotParseEntitiesOver7ArabicDigitsLong()
+        {
+            // ToDo: Remove this test once https://github.com/commonmark/commonmark-spec/pull/575 is merged
+            TestParser.TestSpec("&#87654321;", "<p>&amp;#87654321;</p>");
         }
     }
 }
