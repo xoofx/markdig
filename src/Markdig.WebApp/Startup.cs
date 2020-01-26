2e1912a23d38d4b43bbce64c88b4be0dc8dba344
Alexandre Mutel <alexandre_mutel@live.com>
2020-01-26 19:34:45 +0100

    Update projects to compile with dotnet build/test, remove uap10.0

--- src/Markdig.WebApp/Startup.cs
+++ src/Markdig.WebApp/Startup.cs
@@ -43,8 +43,8 @@ namespace Markdig.WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
             app.UseMvc();
         }
