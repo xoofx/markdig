fc2e1ab89665afa362742b6e23515f5e9a946cac
MihaZupan <mihazupan.zupan1@gmail.com>
2020-04-22 17:48:44 +0200

    Cleanup license headers

--- src/Markdig/Parsers/ParserList.cs
+++ src/Markdig/Parsers/ParserList.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
+
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
--- src/Markdig/Syntax/QuoteBlock.cs
+++ src/Markdig/Syntax/QuoteBlock.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
+
 using Markdig.Parsers;
 
 namespace Markdig.Syntax
