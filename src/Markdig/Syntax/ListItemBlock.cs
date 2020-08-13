d35b1b82eaf56a98b257bdd16a544a9190bfbe13
JiayinPei <jipe@microsoft.com>
2020-08-13 14:34:07 +0800

    Expose ColumnWidth of ListBlockItem

--- src/Markdig/Syntax/ListItemBlock.cs
+++ src/Markdig/Syntax/ListItemBlock.cs
@@ -20,7 +20,7 @@ namespace Markdig.Syntax
         {
         }
 
-        internal int ColumnWidth { get; set; }
+        public int ColumnWidth { get; set; }
 
         /// <summary>
         /// The number defined for this <see cref="ListItemBlock"/> in an ordered list
