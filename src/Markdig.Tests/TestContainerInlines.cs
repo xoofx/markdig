b30b219237c83384622dd249fa1ad90404520f70
Yufei Huang <yufeih@live.com>
2020-08-18 15:42:29 +0800

    Add ParentBlock property to ContainerInline

---
+++ src/Markdig.Tests/TestContainerInlines.cs
@@ -0,0 +1,41 @@
+using System;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+using NUnit.Framework;
+
+namespace Markdig.Tests
+{
+    public class TestContainerInlines
+    {
+        private class MockLeafBlock : LeafBlock
+        {
+            public MockLeafBlock()
+                : base(null)
+            {
+
+            }
+        }
+
+        [Test]
+        public void CanBeAddedToLeafBlock()
+        {
+            var leafBlock1 = new MockLeafBlock();
+
+            var one = new ContainerInline();
+            Assert.Null(one.ParentBlock);
+
+            leafBlock1.Inline = one;
+            Assert.AreSame(leafBlock1, one.ParentBlock);
+
+            var two = new ContainerInline();
+            Assert.Null(two.ParentBlock);
+
+            leafBlock1.Inline = two;
+            Assert.AreSame(leafBlock1, two.ParentBlock);
+            Assert.Null(one.ParentBlock);
+
+            var leafBlock2 = new MockLeafBlock();
+            Assert.Throws<ArgumentException>(() => leafBlock2.Inline = two);
+        }
+    }
+}
