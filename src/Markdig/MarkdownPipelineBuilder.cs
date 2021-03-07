bb5403c795b20b9c88e06b55d3aad389081465b3
MihaZupan <mihazupan.zupan1@gmail.com>
2021-03-07 19:13:34 +0100

    Hide TrackTrivia from PipelineBuilder
    EnableTrackTrivia is exposed to enable trivia

--- src/Markdig/MarkdownPipelineBuilder.cs
+++ src/Markdig/MarkdownPipelineBuilder.cs
@@ -80,7 +80,7 @@ namespace Markdig
         /// True to parse trivia such as whitespace, extra heading characters and unescaped
         /// string values.
         /// </summary>
-        public bool TrackTrivia { get; set; }
+        internal bool TrackTrivia { get; set; }
 
         /// <summary>
         /// Occurs when a document has been processed after the <see cref="MarkdownParser.Parse()"/> method.
