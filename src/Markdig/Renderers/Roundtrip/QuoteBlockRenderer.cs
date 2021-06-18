a6df957e785596e6d5f8bcde6b552fa6f2f8153f
Alexandre Mutel <alexandre_mutel@live.com>
2021-06-18 06:20:01 +0200

    Fix QuoteBlockRenderer to allow to render without QuoteLines. Also stop modifying AST while rendering.

--- src/Markdig/Renderers/Roundtrip/QuoteBlockRenderer.cs
+++ src/Markdig/Renderers/Roundtrip/QuoteBlockRenderer.cs
@@ -2,6 +2,7 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
+using System;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 
@@ -17,42 +18,53 @@ namespace Markdig.Renderers.Roundtrip
             renderer.RenderLinesBefore(quoteBlock);
             renderer.Write(quoteBlock.TriviaBefore);
 
-            var indents = new string[quoteBlock.QuoteLines.Count];
-            for (int i = 0; i < quoteBlock.QuoteLines.Count; i++)
+            var quoteLines = quoteBlock.QuoteLines;
+
+            bool noChildren = quoteBlock.Count == 0;
+
+            if (quoteLines.Count > 0)
             {
-                var quoteLine = quoteBlock.QuoteLines[i];
-                var wsb = quoteLine.TriviaBefore.ToString();
-                var quoteChar = quoteLine.QuoteChar ? ">" : "";
-                var spaceAfterQuoteChar = quoteLine.HasSpaceAfterQuoteChar ? " " : "";
-                var wsa = quoteLine.TriviaAfter.ToString();
-                indents[i] = (wsb + quoteChar + spaceAfterQuoteChar + wsa);
-            }
-            bool noChildren = false;
-            if (quoteBlock.Count == 0)
-            {
-                noChildren = true;
-                // since this QuoteBlock instance has no children, indents will not be rendered. We
-                // work around this by adding empty LineBreakInlines to a ParagraphBlock.
-                // Wanted: a more elegant/better solution (although this is not *that* bad).
-                foreach (var quoteLine in quoteBlock.QuoteLines)
+                var indents = new string[quoteLines.Count];
+                for (int i = 0; i < quoteLines.Count; i++)
                 {
-                    var emptyLeafBlock = new ParagraphBlock
+                    var quoteLine = quoteLines[i];
+                    var wsb = quoteLine.TriviaBefore.ToString();
+                    var quoteChar = quoteLine.QuoteChar ? ">" : "";
+                    var spaceAfterQuoteChar = quoteLine.HasSpaceAfterQuoteChar ? " " : "";
+                    var wsa = quoteLine.TriviaAfter.ToString();
+                    indents[i] = (wsb + quoteChar + spaceAfterQuoteChar + wsa);
+                }
+
+                renderer.PushIndent(indents);
+
+                if (noChildren)
+                {
+                    //// since this QuoteBlock instance has no children, indents will not be rendered. We
+                    //// work around this by rendering explicit empty lines
+                    foreach(var quoteLine in quoteLines)
                     {
-                        NewLine = quoteLine.NewLine
-                    };
-                    var newLine = new LineBreakInline
-                    {
-                        NewLine = quoteLine.NewLine
-                    };
-                    var container = new ContainerInline();
-                    container.AppendChild(newLine);
-                    emptyLeafBlock.Inline = container;
-                    quoteBlock.Add(emptyLeafBlock);
+                        renderer.WriteLine(quoteLine.NewLine);
+                    }
+                }
+                else
+                {
+                    renderer.WriteChildren(quoteBlock);
+                }
+            }
+            else
+            {
+                renderer.PushIndent($"{quoteBlock.QuoteChar} ");
+
+                if (noChildren)
+                {
+                    renderer.WriteLine();
+                }
+                else
+                {
+                    renderer.WriteChildren(quoteBlock);
                 }
             }
 
-            renderer.PushIndent(indents);
-            renderer.WriteChildren(quoteBlock);
             renderer.PopIndent();
 
             if (!noChildren)
