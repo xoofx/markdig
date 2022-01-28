fbd7d83cdac77bf3b3ee184cf6b80f9ce631ed1e
Jason Nelson <jason@carbonmade.com>
2022-01-28 11:50:34 -0800

    Future proof NETCORE pragmas

--- src/Markdig/Helpers/CharacterMap.cs
+++ src/Markdig/Helpers/CharacterMap.cs
@@ -107,7 +107,7 @@ namespace Markdig.Helpers
         {
             if (nonAsciiMap is null)
             {
-#if NETCOREAPP3_1
+#if NETCOREAPP3_1_OR_GREATER
                 ref char textRef = ref Unsafe.AsRef(in text.GetPinnableReference());
                 for (; start <= end; start++)
                 {
@@ -154,7 +154,7 @@ namespace Markdig.Helpers
 
         private int IndexOfOpeningCharacterNonAscii(string text, int start, int end)
         {
-#if NETCOREAPP3_1
+#if NETCOREAPP3_1_OR_GREATER
             ref char textRef = ref Unsafe.AsRef(in text.GetPinnableReference());
             for (int i = start; i <= end; i++)
             {
