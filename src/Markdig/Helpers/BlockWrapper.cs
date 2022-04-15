be3c93a9b06b07ce02105b5abf256da7078414f9
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-04-15 16:21:39 +0200

    Remove trailing space

--- src/Markdig/Helpers/BlockWrapper.cs
+++ src/Markdig/Helpers/BlockWrapper.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Alexandre Mutel. All rights reserved.
-// This file is licensed under the BSD-Clause 2 license. 
+// This file is licensed under the BSD-Clause 2 license.
 // See the license.txt file in the project root for more information.
 
 using Markdig.Syntax;
