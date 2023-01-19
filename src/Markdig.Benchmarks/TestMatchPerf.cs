ca03b7df4f6df1309a149a3503fc1e8d3245c083
Jason Nelson <jason@carbonmade.com>
2023-01-18 19:50:40 -0800

    [Benchmarks] Enable ImplictUsings and use file-scoped namespaces

--- src/Markdig.Benchmarks/TestMatchPerf.cs
+++ src/Markdig.Benchmarks/TestMatchPerf.cs
@@ -2,81 +2,79 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
-using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Testamina.Markdig.Benchmarks
+namespace Testamina.Markdig.Benchmarks;
+
+public class TextRegexHelper
 {
-    public class TextRegexHelper
+    private readonly Dictionary<string, string> replacers;
+    private readonly Regex regex;
+
+    public TextRegexHelper(Dictionary<string, string> replacers)
     {
-        private readonly Dictionary<string, string> replacers;
-        private readonly Regex regex;
+        this.replacers = replacers;
+        var builder = new StringBuilder();
 
-        public TextRegexHelper(Dictionary<string, string> replacers)
+        // (?<1>:value:?)|(?<1>:noo:?)
+        foreach (var replace in replacers)
         {
-            this.replacers = replacers;
-            var builder = new StringBuilder();
-
-            // (?<1>:value:?)|(?<1>:noo:?)
-            foreach (var replace in replacers)
+            var matchStr = Regex.Escape(replace.Key);
+            if (builder.Length > 0)
             {
-                var matchStr = Regex.Escape(replace.Key);
-                if (builder.Length > 0)
-                {
-                    builder.Append('|');
-                }
-                builder.Append("(?<1>").Append(matchStr).Append("?)");
+                builder.Append('|');
             }
-
-            regex = new Regex(builder.ToString());
+            builder.Append("(?<1>").Append(matchStr).Append("?)");
         }
 
-        public bool TryMatch(string text, int offset, out string matchText, out string replaceText)
-        {
-            replaceText = null;
-            matchText = null;
-            var result = regex.Match(text, offset);
-            if (!result.Success)
-            {
-                return false;
-            }
-
-            matchText = result.Groups[1].Value;
-            replaceText = replacers[matchText];
-            return true;
-        }
+        regex = new Regex(builder.ToString());
     }
 
-    /*
-    public class TestMatchPerf
+    public bool TryMatch(string text, int offset, out string matchText, out string replaceText)
     {
-        private readonly TextMatchHelper matcher;
-
-        public TestMatchPerf()
+        replaceText = null;
+        matchText = null;
+        var result = regex.Match(text, offset);
+        if (!result.Success)
         {
-            var replacers = new Dictionary<string, string>();
-            for (int i = 0; i < 1000; i++)
-            {
-                replacers.Add($":z{i}:", i.ToString());
-            }
-            replacers.Add(":abc:", "yes");
-
-            matcher = new TextMatchHelper(new HashSet<string>(replacers.Keys));
+            return false;
         }
 
-        [Benchmark]
-        public void TestMatch()
-        {
-
-            for (int i = 0; i < 1000; i++)
-            {
-                string matchText;
-                //var text = ":z150: this is a long string";
-                var text = ":z1:";
-                matcher.TryMatch(text, 0, text.Length, out matchText);
-            }
-        }
+        matchText = result.Groups[1].Value;
+        replaceText = replacers[matchText];
+        return true;
     }
-    */
 }
+
+/*
+public class TestMatchPerf
+{
+    private readonly TextMatchHelper matcher;
+
+    public TestMatchPerf()
+    {
+        var replacers = new Dictionary<string, string>();
+        for (int i = 0; i < 1000; i++)
+        {
+            replacers.Add($":z{i}:", i.ToString());
+        }
+        replacers.Add(":abc:", "yes");
+
+        matcher = new TextMatchHelper(new HashSet<string>(replacers.Keys));
+    }
+
+    [Benchmark]
+    public void TestMatch()
+    {
+
+        for (int i = 0; i < 1000; i++)
+        {
+            string matchText;
+            //var text = ":z150: this is a long string";
+            var text = ":z1:";
+            matcher.TryMatch(text, 0, text.Length, out matchText);
+        }
+    }
+}
+*/
