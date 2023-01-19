46604267194b2ce4e23c7cbc551714ec8db2de95
Jason Nelson <jason@carbonmade.com>
2023-01-18 19:49:44 -0800

    [WebApp] Enable ImplictUsings and use file-scoped namespaces

--- src/Markdig.WebApp/ApiController.cs
+++ src/Markdig.WebApp/ApiController.cs
@@ -1,55 +1,54 @@
-using System;
 using System.Text;
+
 using Microsoft.AspNetCore.Mvc;
 
-namespace Markdig.WebApp
+namespace Markdig.WebApp;
+
+public class ApiController : Controller
 {
-    public class ApiController : Controller
+    [HttpGet()]
+    [Route("")]
+    public string Empty()
     {
-        [HttpGet()]
-        [Route("")]
-        public string Empty()
+        return string.Empty;
+    }
+
+    // GET api/to_html?text=xxx&extension=advanced
+    [Route("api/to_html")]
+    [HttpGet()]
+    public object Get([FromQuery] string text, [FromQuery] string extension)
+    {
+        try
         {
-            return string.Empty;
-        }
+            if (text == null)
+            {
+                text = string.Empty;
+            }
 
-        // GET api/to_html?text=xxx&extension=advanced
-        [Route("api/to_html")]
-        [HttpGet()]
-        public object Get([FromQuery] string text, [FromQuery] string extension)
+            if (text.Length > 1000)
+            {
+                text = text.Substring(0, 1000);
+            }
+
+            var pipeline = new MarkdownPipelineBuilder().Configure(extension).Build();
+            var result = Markdown.ToHtml(text, pipeline);
+
+            return new {name = "markdig", html = result, version = Markdown.Version};
+        }
+        catch (Exception ex)
         {
-            try
-            {
-                if (text == null)
-                {
-                    text = string.Empty;
-                }
-
-                if (text.Length > 1000)
-                {
-                    text = text.Substring(0, 1000);
-                }
-
-                var pipeline = new MarkdownPipelineBuilder().Configure(extension).Build();
-                var result = Markdown.ToHtml(text, pipeline);
-
-                return new {name = "markdig", html = result, version = Markdown.Version};
-            }
-            catch (Exception ex)
-            {
-                return new { name = "markdig", html = "exception: " + GetPrettyMessageFromException(ex), version = Markdown.Version };
-            }
+            return new { name = "markdig", html = "exception: " + GetPrettyMessageFromException(ex), version = Markdown.Version };
         }
+    }
 
-        private static string GetPrettyMessageFromException(Exception exception)
+    private static string GetPrettyMessageFromException(Exception exception)
+    {
+        var builder = new StringBuilder();
+        while (exception != null)
         {
-            var builder = new StringBuilder();
-            while (exception != null)
-            {
-                builder.Append(exception.Message);
-                exception = exception.InnerException;
-            }
-            return builder.ToString();
+            builder.Append(exception.Message);
+            exception = exception.InnerException;
         }
+        return builder.ToString();
     }
 }
--- src/Markdig.WebApp/Startup.cs
+++ src/Markdig.WebApp/Startup.cs
@@ -1,60 +1,52 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+namespace Markdig.WebApp;
 
-namespace Markdig.WebApp
+public class Startup
 {
-    public class Startup
+    public Startup(IWebHostEnvironment env)
     {
-        public Startup(IWebHostEnvironment env)
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+        if (env.IsEnvironment("Development"))
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-            if (env.IsEnvironment("Development"))
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-            }
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+            builder.AddApplicationInsightsSettings(developerMode: true);
         }
 
-        public IConfigurationRoot Configuration { get; }
+        builder.AddEnvironmentVariables();
+        Configuration = builder.Build();
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
+    public IConfigurationRoot Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        // Add framework services.
+        services.AddApplicationInsightsTelemetry(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+        if (env.IsDevelopment())
         {
-            services.AddControllers();
-            
-            // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
+            app.UseDeveloperExceptionPage();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
