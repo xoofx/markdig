ba1e562d2fe7418efeb53f466712bbd6ad9c39f9
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2023-12-12 16:15:55 +0800

    Fix complication error of `StringLineGroup`.

--- src/Markdig/Helpers/StringLineGroup.cs
+++ src/Markdig/Helpers/StringLineGroup.cs
@@ -218,7 +218,7 @@ public struct StringLineGroup : IEnumerable
 
     IEnumerator IEnumerable.GetEnumerator() 
     {
-        (IEnumerator)GetEnumerator();
+        return GetEnumerator();
     }
 
     private void IncreaseCapacity()
