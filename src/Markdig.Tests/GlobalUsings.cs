f52ecee0b995200e234dc0fc275e2dcdedcdfca0
Alexandre Mutel <alexandre_mutel@live.com>
2023-12-14 06:24:19 +0100

    Update packages

---
+++ src/Markdig.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+global using Assert = NUnit.Framework.Legacy.ClassicAssert;
