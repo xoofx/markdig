9dac60df7358e2b4fa3108a00bf459b64d3cbaf3
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2025-02-24 10:49:59 +0800

    Replace encoding polyfill with NET5+ one.
    netstandard2.1 is a special TFM that .NET5+ doesn't mark themselves compitable, even if they mostly are.

--- src/Markdig/Polyfills/EncodingExtensions.cs
+++ src/Markdig/Polyfills/EncodingExtensions.cs
@@ -2,7 +2,7 @@
 // This file is licensed under the BSD-Clause 2 license.
 // See the license.txt file in the project root for more information.
 
-#if !NETSTANDARD2_1_OR_GREATER
+#if !NETCOREAPP2_1_OR_GREATER && !NETSTANDARD2_1_OR_GREATER
 
 using System.Runtime.InteropServices;
 
