ee403ce28f1a18a1216e43fdfb3344c45b3f813f
James Newton-King <james@newtonking.com>
2025-03-17 08:26:51 +0800

    Port tests

--- src/Markdig.Tests/TestAutoLinks.cs
+++ src/Markdig.Tests/TestAutoLinks.cs
@@ -10,6 +10,8 @@ public class TestAutoLinks
     [TestCase("http://localhost", "<p><a href=\"http://localhost\">http://localhost</a></p>")]
     [TestCase("https://l", "<p><a href=\"https://l\">https://l</a></p>")]
     [TestCase("www.l", "<p><a href=\"http://www.l\">www.l</a></p>")]
+    [TestCase("https://localhost:5000", "<p><a href=\"https://localhost:5000\">https://localhost:5000</a></p>")]
+    [TestCase("www.l:5000", "<p><a href=\"http://www.l:5000\">www.l:5000</a></p>")]
     public void TestLinksWithAllowDomainWithoutPeriod(string markdown, string expected)
     {
         var pipeline = new MarkdownPipelineBuilder()
